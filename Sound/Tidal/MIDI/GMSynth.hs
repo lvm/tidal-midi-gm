b94274e2df129739e324b5acc6a58ea2327015e8
Mauro <lavaramano@gmail.com>
2016-07-10 18:59:33 -0300

    adding general midi synth (which has higher latency than synth.hs)

---
+++ Sound/Tidal/MIDI/GMSynth.hs
@@ -0,0 +1,21 @@
+module Sound.Tidal.MIDI.GMSynth where
+
+import Sound.Tidal.Params
+import Sound.Tidal.MIDI.Control
+
+synthController :: ControllerShape
+synthController = ControllerShape {
+  controls = [
+    mCC modwheel_p 1,
+    mCC pan_p 10,
+    mCC balance_p 8,
+    mCC expression_p 11,
+    mCC sustainpedal_p 64,
+    mCC portamento_p 5
+    ],
+  latency = 0.3
+  }
+
+synth = toShape synthController
+
+(balance, balance_p) = pF "balance" (Just 0)
