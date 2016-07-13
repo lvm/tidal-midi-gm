d09b4afa2ed3fdf3f7975726566c42dd73d02231
Mauro <lavaramano@gmail.com>
2016-07-13 11:13:45 -0300

    oops, missing import

--- Sound/Tidal/MIDI/GMParams.hs
+++ Sound/Tidal/MIDI/GMParams.hs
@@ -1,5 +1,7 @@
 module Sound.Tidal.MIDI.GMParams where
 
+import Sound.Tidal.Params
+
 (balance, balance_p) = pF "balance" (Just 0)
 (reverb, reverb_p) = pF "reverb" (Just 0)
 (chorus, chorus_p) = pF "chorus" (Just 0)
